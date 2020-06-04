459c191a383f0c6b8f0ebbf5dc50723ad620e898
chakbun <chakbunhuang@gmail.com>
2020-06-04 16:09:46 +0800

    0604---

---
+++ src/main/java/com/cykj/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.cykj.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller("hello")
+public class HelloController {
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index(){
+        return "hello world";
+    }
+
+}
